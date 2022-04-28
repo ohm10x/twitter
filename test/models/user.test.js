380bd4c5fa8daf34b21f04b979c0817a312f26af
omar <omar22dp@gmail.com>
2022-04-28 17:21:23 -0500

    Agregando prueba de los requerimientos

--- test/models/user.test.js
+++ test/models/user.test.js
@@ -2,7 +2,7 @@ const User = require('./../../app/models/user')
 
 describe("Unit Tests for User class", () => {
 
-	test(' Create an User object', () => {
+	test('1) Create an User object', () => {
         const user = new User(1, "omardrnp", "Omar", "Bio")
             expect(user.id).toBe(1)
             expect(user.username).toBe("omardrnp")
@@ -11,5 +11,23 @@ describe("Unit Tests for User class", () => {
             expect(user.dateCreated).not.toBeUndefined()
             expect(user.lastUpdated).not.toBeUndefined()
         });
+     
+        test('2) Add getters', () => {
+            const user = new User(1, "omardrnp", "Omar", "Bio")
+            expect(user.getUsername).toBe("omardrnp")
+            expect(user.getBio).toBe("Bio")
+            expect(user.getDateCreated).not.toBeUndefined()
+            expect(user.getLastUpdated).not.toBeUndefined()
+        });        
 
+
+        test('3) Add setters', () => {
+            const user = new User(1, "omardrnp", "Omar", "Bio")
+            user.setUsername = "Duran"
+            expect(user.username).toBe("Duran")
+            user.setBio = "New bio"
+            expect(user.bio).toBe("New bio")
+            });
+
+        
 })
