2144292da95bfc1600da8e81c38ab920fd180d14
omar <omar22dp@gmail.com>
2022-04-28 16:15:31 -0500

    Modificando el archivo user.tets.js

--- test/models/user.test.js
+++ test/models/user.test.js
@@ -3,11 +3,13 @@ const User = require('./../../app/models/user')
 describe("Unit Tests for User class", () => {
 
 	test(' Create an User object', () => {
-        const user = new User(1, "carlogilmar", "Carlo", "Bio")
+        const user = new User(1, "omardrnp", "Omar", "Bio")
             expect(user.id).toBe(1)
-            expect(user.username).toBe("carlogilmar")
-            expect(user.name).toBe("Carlo")
+            expect(user.username).toBe("omardrnp")
+            expect(user.name).toBe("Omar")
             expect(user.bio).toBe("Bio")
+            expect(user.dateCreated).not.toBeUndefined()
+            expect(user.lastUpdated).not.toBeUndefined()
         });
 
 })
